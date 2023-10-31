48d845e1e9a801b68e18d4971dc6948034764b68
Alexander <148794699+alexanderrothschild@users.noreply.github.com>
2023-10-31 15:21:23 +0100

    create html/css/js

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-ReactDOM.render(<h1>DashBoard</h1>, document.getElementById("root"))
+import 'bootstrap/dist/css/bootstrap.css';
+ReactDOM.render(<h1>Dashboard</h1>, document.getElementById("root"))
 ReactDOM.render(<p>Testing the Dashboard</p>, document.getElementById("infoText"))
+ReactDOM.render(
+    <ul>
+        <li>Module 1</li>
+        <li>Module 2</li>
+        <li>Module 3</li>
+    </ul>,
+    document.getElementById("moduleList"))
+
